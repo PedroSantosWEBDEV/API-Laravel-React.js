0a71991da6ed924aafe2dbdaa9639392c2cf5ae3
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2023-01-09 16:06:04 -0300

    Backend Change

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\UserController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Middleware\JwtMiddleware;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,15 +12,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-// Route::post('register', 'UserController@store'); 
-Route::controller(UserController::class)->group(function () {
-  Route::post('register', 'store')->name('users.store');
-  Route::post('login', 'login')->name('users.login');
-  Route::post('logout', 'logout')->name('users.logout');
-
+Route::post('register', [UserController::class, 'store'])->name('users.store');
+Route::post('login', [UserController::class, 'login'])->name('users.login');
+Route::prefix('v1')->group(function () {
+        Route::post('logout', [UserController::class, 'logout'])->name('users.logout')->middleware(JwtMiddleware::class);
 });
-// Route::post('register', [UserController::class, 'store'])->name('users.store');
-// Route::post('login', [UserController::class, 'login'])->name('users.login');
-// Route::post('logout', [UserController::class, 'logout'])->name('users.logout');
