f66aa2b0e0be8c0c486b7457213216737200fb1a
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2023-01-09 23:33:33 -0300

    Backend Change

--- backend/app/Http/Controllers/TaskListController.php
+++ backend/app/Http/Controllers/TaskListController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\TaskList;
-use App\Http\Requests\StoreTaskListRequest;
-use App\Http\Requests\UpdateTaskListRequest;
+use App\Services\ResponseService;
+use App\Transformers\TaskList\TaskListResource;
+use App\Transformers\TaskList\TaskListResourceCollection;
+use Illuminate\Http\Request;
 
 class TaskListController extends Controller
 {
+    private $tasklist;
+
+    public function __construct(TaskList $tasklist)
+    {
+        $this->tasklist = $tasklist;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,72 +24,81 @@ class TaskListController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return new TaskListResourceCollection($this->tasklist->index());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreTaskListRequest  $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreTaskListRequest $request)
+    public function store(StoreTaskList $request)
     {
-        //
+        try {
+            $data = $this
+                ->tasklist
+                ->create($request->all());
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasklist.store', null, $e);
+        }
+
+        return new TaskListResource($data, array('type' => 'store', 'route' => 'tasklist.store'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\TaskList  $taskList
+     * @param  \App\TaskList  $taskList
      * @return \Illuminate\Http\Response
      */
-    public function show(TaskList $taskList)
+    public function show($id)
     {
-        //
-    }
+        try {
+            $data = $this
+                ->tasklist
+                ->show($id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasklist.show', $id, $e);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\TaskList  $taskList
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(TaskList $taskList)
-    {
-        //
+        return new TaskListResource($data, array('type' => 'show', 'route' => 'tasklist.show'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateTaskListRequest  $request
-     * @param  \App\Models\TaskList  $taskList
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateTaskListRequest $request, TaskList $taskList)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $data = $this
+                ->tasklist
+                ->updateList($request->all(), $id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasklist.update', $id, $e);
+        }
+
+        return new TaskListResource($data, array('type' => 'update', 'route' => 'tasklist.update'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\TaskList  $taskList
+     * @param  \App\TaskList  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(TaskList $taskList)
+    public function destroy($id)
     {
-        //
+        try {
+            $data = $this
+                ->tasklist
+                ->destroyList($id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasklist.destroy', $id, $e);
+        }
+        return new TaskListResource($data, array('type' => 'destroy', 'route' => 'tasklist.destroy'));
     }
 }
--- backend/app/Models/TaskList.php
+++ backend/app/Models/TaskList.php
@@ -3,17 +3,63 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class TaskList extends Model
 {
-    use HasFactory;
+    protected $fillable = ['user_id','title','status'];
+
+    public function index(){
+        return auth()
+        ->user()
+        ->TaskList
+        ->sortBy("status");
+    }
+
+    public function create($fields)
+    {
+        return auth()
+        ->user()
+        ->TaskList()
+        ->create($fields);
+    }
+
+    public function show($id)
+    {
+        $show = auth()
+        ->user()
+        ->TaskList()
+        ->find($id);
+ 
+        if (!$show) {
+            throw new \Exception('Nada Encontrado', -404);
+        }
+
+        return $show;
+    }
+
+    public function updateList($fields, $id)
+    {
+        $tasklist = $this->show($id);
+
+        $tasklist->update($fields);
+        return $tasklist;
+    }
+
+    public function destroyList($id)
+    {
+        $tasklist = $this->show($id);
+
+        $tasklist->delete();
+        return $tasklist;
+    }
 
     public function user(){
-        return $this->belongsTo('App\Models\User');
-      }
-      public function tasks(){
-        return $this->hasMany('App\Models\Tasks', 'user_id', 'user_id',     
-        'list_id', 'id');
-      }
+        return $this->belongsTo('App\User');
+    }
+
+    public function tasks(){
+        return $this->hasMany('App\Tasks');
+    }
 }
