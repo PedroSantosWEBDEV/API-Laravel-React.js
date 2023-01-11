9681fb092e62e002a742e11a68cf8538d64a1117
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2023-01-11 08:46:46 -0300

    backend change

--- backend/app/Http/Controllers/TasksController.php
+++ backend/app/Http/Controllers/TasksController.php
@@ -2,85 +2,143 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Task\StoreTask;
 use App\Models\Tasks;
-use App\Http\Requests\StoreTasksRequest;
-use App\Http\Requests\UpdateTasksRequest;
+use App\Services\ResponseService;
+use App\Transformers\Tasks\TasksResource;
+use App\Transformers\Tasks\TasksResourceCollection;
+use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
+    private $tasks;
+
+    public function __construct(Tasks $tasks)
+    {
+        $this->tasks = $tasks;
+    }
+
     /**
      * Display a listing of the resource.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return new TasksResourceCollection($this->tasks->index());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreTasksRequest  $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreTasksRequest $request)
+    public function store(StoreTask $request)
     {
-        //
+        try {
+            $data = $this
+                ->tasks
+                ->store($request->all());
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasks.store', null, $e);
+        }
+
+        return new TasksResource($data, array('type' => 'store', 'route' => 'tasks.store'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Tasks  $tasks
+     * @param  \App\Tasks  $tasks
      * @return \Illuminate\Http\Response
      */
-    public function show(Tasks $tasks)
+    public function show($id)
     {
-        //
+        try {
+            $data = $this
+                ->tasks
+                ->show($id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasks.show', $id, $e);
+        }
+
+        return new TasksResource($data, array('type' => 'show', 'route' => 'tasks.show'));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Display the specified resource.
      *
-     * @param  \App\Models\Tasks  $tasks
+     * @param  \App\Tasks  $tasks
      * @return \Illuminate\Http\Response
      */
-    public function edit(Tasks $tasks)
+    public function tasksByList($id)
     {
-        //
+        try {
+            $data = $this
+                ->tasks
+                ->tasksByList($id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasks.tasksByList', $id, $e);
+        }
+
+        return new TasksResourceCollection($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tasks  $tasks
+     * @return \Illuminate\Http\Response
+     */
+    public function closeTask($id)
+    {
+        try {
+            $data = $this
+                ->tasks
+                ->closeTask($id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasks.closeTask', $id, $e);
+        }
+
+        return new TasksResource($data, array('type' => 'update', 'route' => 'tasks.closeTask'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateTasksRequest  $request
-     * @param  \App\Models\Tasks  $tasks
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateTasksRequest $request, Tasks $tasks)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $data = $this
+                ->tasks
+                ->updateTask($request->all(), $id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasks.update', $id, $e);
+        }
+
+        return new TasksResource($data, array('type' => 'update', 'route' => 'tasks.update'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Tasks  $tasks
+     * @param  \App\Tasks  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Tasks $tasks)
+    public function destroy($id)
     {
-        //
+        try {
+            $data = $this
+                ->tasks
+                ->destroyTask($id);
+        } catch (\Throwable | \Exception$e) {
+            return ResponseService::exception('tasks.destroy', $id, $e);
+        }
+        return new TasksResource($data, array('type' => 'destroy', 'route' => 'tasks.destroy'));
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskListController;
 use App\Http\Controllers\TasksController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [UserController::class, 'store'])->name('users.store');
 Route::post('login', [UserController::class, 'login'])->name('users.login');
+Route::post('completedTaskList', [TaskListController::class, 'completedTaskList'])->name('tasklist.completedTaskList');
+Route::put('task/close/{id}', [TasksController::class, 'closeTask'])->name('tasks.closeTask');
+Route::get('list/tasks/{id}', [TasksController::class, 'tasksByList'])->name('tasks.tasksByList');
+
 Route::prefix('v1')->group(function () {
-
-    Route::apiResources(['tasklist' => 'TaskListController',]);
-
-    Route::post('completedTaskList', [TaskListController::class, 'completedTaskList'])->name('tasklist.completedTaskList');
+    // Route::apiResources([
+    //     'tasklist' => 'TaskListController',
+    //     'tasks' => 'TasksController',
+    // ]);
     Route::post('logout', [UserController::class, 'logout'])->name('users.logout')->middleware(JwtMiddleware::class);
 
 });
