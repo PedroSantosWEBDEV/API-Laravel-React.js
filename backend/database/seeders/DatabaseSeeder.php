10fef57b138d821f5a30f95304ac600810ca1183
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2023-01-11 10:40:33 -0300

    backend change

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,16 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = DB::table('users')->insertGetId([
+                'name'      => 'Admin',
+                'email'     => 'teste@teste.com',
+                'password'  => '$2y$10$xMD9DsNkvTE6i8KdG8quge/Qk3K2EfMZQIQYcaa.LyFS8pp2tNHfq',
+                'remember_token' => '',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call([
-            UserSeeder::class,
-        ]);
+            $TaskList = DB::table('task_lists')->insertGetId([
+                'user_id' => $user,
+                'title' => 'Fake',
+                'status' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
 
-        $this->call(TaskListSeeder::class);
+            $Task = DB::table('tasks')->insertGetId([
+                        'user_id' => $user,
+                        'list_id'=> $TaskList,
+                        'title' => 'Fake',
+                        'status' => 0,
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now()
+                    ]);
     }
 }
