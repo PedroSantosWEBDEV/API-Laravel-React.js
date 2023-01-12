385ae7b98b8c54d86f6edf791d19a08a68bffd17
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2023-01-12 03:27:38 -0300

    front end change

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Lists from './pages/Lists';
 
-export default function Routes() {
+export default function Rotas() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Logon} />
-        <Route path="/register" component={Register} />
-        <Route path="/lists" component={Lists} />
-      </Switch>
+      <Routes>
+        <Route path="/" exact element={<Logon/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/lists" element={<Lists/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
